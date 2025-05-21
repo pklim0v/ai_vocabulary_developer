0ece666d958725e5b58098dc1ecd1e46b7577e1b
pklim0v <127421213+pklim0v@users.noreply.github.com>
2025-05-21 20:18:23 +0300

    Add environment-specific configurations
    Introduced separate classes for Development, Testing, and Production configurations, each with tailored LOG_LEVEL and DATABASE_URI settings. This enhances flexibility and simplifies environment-specific setup.

--- config.py
+++ config.py
@@ -5,6 +5,22 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 class Config:
     CLAUDE_API_TOKEN = os.environ.get('CLAUDE_API_TOKEN')
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-    LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
+
+class DevelopmentConfig(Config):
+    LOG_LEVEL = 'DEBUG'
     DATABASE_URI=f'sqlite+aiosqlite://{BASE_DIR}/database/database.sql'
+
+class TestingConfig(Config):
+    LOG_LEVEL = 'DEBUG'
+    DATABASE_URI=f'sqlite+aiosqlite://{BASE_DIR}/database/database_test.sql'
+
+class ProductionConfig(Config):
+    LOG_LEVEL = 'INFO'
+    DATABASE_URI=f'sqlite+aiosqlite://{BASE_DIR}/database/database.sql'
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
